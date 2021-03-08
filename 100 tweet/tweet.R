90172cc8babba3b4aff43980f62d7b64a7e30fe9
Thiago Santos <tdos233@gmail.com>
2021-03-07 21:45:25 -0300

    tirando x antes dos ids

--- 100 tweet/tweet.R
+++ 100 tweet/tweet.R
@@ -3,5 +3,6 @@ library(dplyr)
 
 tweet <- search_tweets("#vacina", n = 100, 
                           include_rts = FALSE)
-write_as_csv(tweet,'tweet.csv')
+tweet<-read_twitter_csv('/home/thiago/Documentos/MATB10-TOPICOS-EM-BANCO-DE-DADOS-NPL/100 tweet/tweet.csv')
 
+rtweet::save_as_csv(tweet,'tweet.csv',prepend_ids = FALSE)
