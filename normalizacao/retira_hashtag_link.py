db93fc0b5403868131079697a5030d38fd26873a
Thiago Santos <tdos233@gmail.com>
2021-03-18 14:17:12 -0300

    tirando hashtag e links

---
+++ normalizacao/retira_hashtag_link.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import re
+
+
+df = pd.read_csv('tweet_text_norm.csv', sep=',', encoding='utf8')
+
+#print(df.rows)
+
+
+df.replace({r'#': ''}, regex=True, inplace=True)
+df.replace({r'https\S+': ''}, regex=True, inplace=True)
+
+df.to_csv('tweet_text_norm_sem_hashtag_link.csv', index=False, encoding='utf8', header=False, line_terminator='\n')
+
+
+for i, row in df.iterrows():
+    print('- ', i, row[0])
+
